1c546a97ae48fcde2638dd3083bd62c273c031d4
Jaydeep Suthar <technicaljade@gmail.com>
2022-02-12 18:07:11 +0530

    Admin Layout Created
    Signed-off-by: Jaydeep Suthar <technicaljade@gmail.com>

---
+++ layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-app id="be-a-programmer">
+		<v-navigation-drawer v-model="drawer" app>
+			<h2>Sidebar</h2>
+		</v-navigation-drawer>
+
+		<v-app-bar app elevate-on-scroll>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+			<v-toolbar-title>Be A Programmer</v-toolbar-title>
+		</v-app-bar>
+
+		<v-main>
+			<v-container>
+				<Nuxt />
+			</v-container>
+		</v-main>
+	</v-app>
+</template>
+
+<script>
+	export default {
+		name: "AdminLayout",
+		data: () => ({ drawer: null }),
+	};
+</script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<h2>Admin Panel</h2>
+</template>
+
+<script>
+	export default {
+		layout: "AdminLayout",
+	};
+</script>
