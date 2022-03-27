70bee27361d0aa99e33236fb197c71a7bc8a5fa7
Jaydeep Suthar <technicaljade@gmail.com>
2022-03-27 14:53:04 +0530

    FAQ Component Completed
    counter is implemented with css couter
    for that some style is added in main.scss

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -1,11 +1,115 @@
 <template>
-	<div id="faq">
-		<p>Frequently Asked Questions</p>
-	</div>
+	<section id="faq" class="mt-5">
+		<div
+			class="
+				faq-title
+				m-0
+				min-w-0
+				text-3xl
+				font-semibold
+				select-none
+				text-left
+				transform-none
+			"
+		>
+			Frequently Asked Questions
+		</div>
+
+		<div class="faq-list mt-5">
+			<li class="faq-list-item list-none select-none hover:no-underline">
+				<p class="faq-question font-bold">
+					When does this course start and finish
+				</p>
+				<p class="faq-answer">
+					The course starts now and never ends. It is a completely
+					self-paced online course. you decide when you start and when
+					you finish.
+				</p>
+			</li>
+
+			<li class="faq-list-item list-none">
+				<p class="faq-question font-bold">
+					What does this course includes?
+				</p>
+				<p class="faq-answer">
+					Course includes online streaming access to videos, all code
+					files that are used in this course. Please check course
+					content on the course page, as we list all of our topics
+					very transparently. Whatever is included, is mentioned.
+				</p>
+			</li>
+			<li class="faq-list-item list-none">
+				<p class="faq-question font-bold">
+					How many times can I watch a video
+				</p>
+				<p class="faq-answer">
+					As many times as you like. Watch it 10 times or 100 times.
+					Totally on you
+				</p>
+			</li>
+			<li class="faq-list-item list-none">
+				<p class="faq-question font-bold">
+					Does this course come with certificate?
+				</p>
+				<p class="faq-answer">
+					Yep, All of our paid course comes with certificates
+				</p>
+			</li>
+			<li class="faq-list-item list-none">
+				<p class="faq-question font-bold">
+					Can I check the video quality?
+				</p>
+				<p class="faq-answer">
+					Yes of course. In fact, we recommend it. Take our free
+					javascript course or take a look. We take audio, video and
+					content quality very seriously. Also, some videos in this
+					course are marked for preview, so that you can test the
+					quality
+				</p>
+			</li>
+			<li class="faq-list-item list-none">
+				<p class="faq-question font-bold">Do you offer Refunds?</p>
+				<p class="faq-answer">
+					We recommend checking free previews and course content.
+					Also, check our free course to get an idea about video and
+					content quality. Our courses are a digital product which
+					means, once consumed, it cannot be returned. WE DON'T OFFER
+					REFUNDS AT THIS PLATFORM. Sorry, we are not a marketplace,
+					where quality can be misled by fake reviews.
+				</p>
+			</li>
+		</div>
+	</section>
 </template>
 
 <script>
 	export default {
-		name: 'FAQ'
-	}
+		name: "FAQ",
+	};
 </script>
+
+<style lang="scss" scoped>
+	.faq-list > * {
+		user-select: none;
+
+		&:hover {
+			text-decoration: none;
+			color: currentColor;
+		}
+	}
+	.faq-question::before {
+		counter-increment: faq-counter;
+		content: counter(faq-counter) ". ";
+	}
+	.faq-answer {
+		box-sizing: border-box;
+		margin: 0px 0px 16px;
+		min-width: 0px;
+		font-size: 1rem;
+		font-weight: 400;
+		user-select: none;
+		text-align: left;
+		text-transform: none;
+		line-height: 1.5;
+	}
+</style>
