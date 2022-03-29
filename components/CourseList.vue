4cac85a86a3034fd4dba4b45e5a52a0ac81ef106
Jaydeep Suthar <technicaljade@gmail.com>
2022-03-29 10:27:54 +0530

    Grid System added to Course List

--- components/CourseList.vue
+++ components/CourseList.vue
@@ -1,33 +1,32 @@
 <template>
-	<div class="course-list d-flex flex-wrap">
-		<div class="course-card" v-for="course in courses" :key="course.id">
-			<v-card
-				width="340px"
-				hover
-				class="ma-2"
-				:to="`/learn/course/${course.id}`"
-			>
-				<v-img
-					:src="`http://localhost:8000/static/img/${course.thumbnail}`"
-					@error="`http://localhost:8000/static/img/nodejs.png`"
-				/>
-				<v-card-title>{{ course.title }}</v-card-title>
-			</v-card>
-		</div>
-	</div>
+	<v-container>
+		<v-row>
+			<v-col v-for="course in courses" :key="course.id" xl="2" lg="3" md="4" sm="6">
+				<v-card
+					width="340px"
+					hover
+					class="ma-2"
+					:to="`/learn/course/${course.id}`"
+				>
+					<v-img
+						:src="`http://localhost:8000/static/img/${course.thumbnail}`"
+						@error="`http://localhost:8000/static/img/nodejs.png`"
+					/>
+					<v-card-title>{{ course.title }}</v-card-title>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-	export default {
-		name: "CourseList",
-		props: ["courses"],
-		methods: {
-			showCourseList() {
-				console.log(this.courses);
-			},
+export default {
+	name: "CourseList",
+	props: ["courses"],
+	methods: {
+		showCourseList() {
+			console.log(this.courses);
 		},
-	};
+	},
+};
 </script>
-
-<style lang="scss" scoped>
-</style>
