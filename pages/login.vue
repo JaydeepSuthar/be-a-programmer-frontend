ba26bae33f475fc58966febdc54e32131cb8982b
Jaydeep Suthar <technicaljade@gmail.com>
2022-04-01 12:12:58 +0530

    User Signup and Login Completed

--- pages/login.vue
+++ pages/login.vue
@@ -1,43 +1,51 @@
 <template>
-  <div>
-    <v-app class="man">
-      <navbar />
-      <h1>Login</h1>
-      <br />
-      <v-layout>
-        <v-flex sm12 md6 offset-md3>
-          <v-card elevation="4" light tag="section">
-            <v-container>
-              <UserAuthForm :submitForm="handleUserLogin" buttonText="Login" />
+	<div>
+		<v-app class="man">
+			<navbar />
+			<h1>Login</h1>
+			<br />
+			<v-layout>
+				<v-flex sm12 md6 offset-md3>
+					<v-card elevation="4" light tag="section">
+						<v-container>
+							<UserAuthForm
+								:submitForm="handleUserLogin"
+								buttonText="Login"
+							/>
 
-              <br />
+							<br />
 
-              <v-btn @click="loginWithGoogle" color="blue">
-                Sign in with Google</v-btn
-              ><br />
-              <br />
+							<!-- <v-btn @click="loginWithGoogle" color="blue">
+								Sign in with Google</v-btn
+							> -->
+							<br />
+							<br />
 
-              <strong style="font-size: 20px">New User? </strong
-              >&nbsp;&nbsp;&nbsp;
-              <v-btn type="submit" large class="success" to="/signup"
-                >Sign up</v-btn
-              >
-            </v-container>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-app>
-    <ftr />
-  </div>
+							<strong style="font-size: 20px">New User? </strong
+							>&nbsp;&nbsp;&nbsp;
+							<v-btn
+								type="submit"
+								large
+								class="success"
+								to="/signup"
+								>Sign up</v-btn
+							>
+						</v-container>
+					</v-card>
+				</v-flex>
+			</v-layout>
+		</v-app>
+		<ftr />
+	</div>
 </template>
 <style scoped>
 h1 {
-  color: rgba(10, 49, 85, 0.959);
-  font-size: 60px;
-  text-align: center;
+	color: rgba(10, 49, 85, 0.959);
+	font-size: 60px;
+	text-align: center;
 }
 .man {
-  background: rgb(199, 199, 199);
+	background: rgb(199, 199, 199);
 }
 </style>
 
@@ -45,31 +53,31 @@ h1 {
 import UserAuthForm from "@/components/UserAuthForm.vue";
 
 export default {
-  components: {
-    UserAuthForm,
-  },
-  methods: {
-    async handleUserLogin(loginInfo) {
-      // * for future reference
-      //   let response = await this.$auth.loginWith("local", {
-      //     data: loginInfo,
-      //   });
-      //   let user = response.data.loggedInUser;
+	components: {
+		UserAuthForm,
+	},
+	methods: {
+		async handleUserLogin(loginInfo) {
+			let response = await this.$axios.post("/user/login", loginInfo);
+			let userData = response.data;
 
-      //   this.$auth.setUser(user);
+			if (
+				response.status == 201 ||
+				response.status == 200 ||
+				response.status == 204
+			) {
+				let user = userData.data;
 
-      let response = await this.$axios.post("/user/login", loginInfo);
-      let userData = response.data;
-      console.log(userData).auth;
+				this.$auth.setUser(user);
 
-      //   let auth_token = userData.auth.token;
-      let user = userData.data;
+				console.log(user);
 
-      console.log(user);
-    },
-    async loginWithGoogle() {
-      let response = await this.$auth.loginWith("google");
-    },
-  },
+				this.$router.push("/");
+			}
+		},
+		// async loginWithGoogle() {
+		// 	let response = await this.$auth.loginWith("google");
+		// },
+	},
 };
 </script>
