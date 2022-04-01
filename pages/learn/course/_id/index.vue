c99413e625b5027ee3f4c248e3e6c408eb9dfb86
Jaydeep Suthar <technicaljade@gmail.com>
2022-04-01 15:28:18 +0530

    Minor Tweaks in Footer, Navbar, Course
    footer removed from courses and it's child
    navbar changed from sticky to normal

--- pages/learn/course/_id/index.vue
+++ pages/learn/course/_id/index.vue
@@ -1,88 +1,92 @@
 <template>
-  <div>
-    <navbar />
-    <v-container id="course-details">
-      <v-row>
-        <v-col id="course-details-content" cols="12" sm="12" md="8">
-          <h2>{{ course.title }}</h2>
+	<div>
+		<v-container id="course-details">
+			<v-row>
+				<v-col id="course-details-content" cols="12" sm="12" md="8">
+					<h2>{{ course.title }}</h2>
 
-          <p>{{ course.description }}</p>
+					<p>{{ course.description }}</p>
 
-          <p></p>
-          <!-- chapters here -->
-          <!-- FIXME: for some reason component is not working so we dont converting it into component -->
-          <!-- <ChapterList :chapters="chapters" /> -->
-          <div class="syllabus">
-            <v-list>
-              <v-list-group v-for="chapter in chapters" :key="chapter.id">
-                <template v-slot:activator>
-                  <v-list-item-title>{{
-                    chapter.chapter_name
-                  }}</v-list-item-title>
-                </template>
+					<p></p>
+					<!-- chapters here -->
+					<!-- FIXME: for some reason component is not working so we dont converting it into component -->
+					<!-- <ChapterList :chapters="chapters" /> -->
+					<div class="syllabus">
+						<v-list>
+							<v-list-group
+								v-for="chapter in chapters"
+								:key="chapter.id"
+							>
+								<template v-slot:activator>
+									<v-list-item-title>{{
+										chapter.chapter_name
+									}}</v-list-item-title>
+								</template>
 
-                <v-list-item v-for="video in chapter.videos" :key="video.id">
-                  {{ video.title }}
-                </v-list-item>
-              </v-list-group>
-            </v-list>
-          </div>
+								<v-list-item
+									v-for="video in chapter.videos"
+									:key="video.id"
+								>
+									{{ video.title }}
+								</v-list-item>
+							</v-list-group>
+						</v-list>
+					</div>
 
-          <div class="instructor-details">
-            <h2>instructor</h2>
+					<div class="instructor-details">
+						<h2>instructor</h2>
 
-            <div class="avtar-with-name">
-              <v-avatar size="36px">
-                <img
-                  alt="Avatar"
-                  src="https://avatars.githubusercontent.com/u/52647252?v=4&s=460"
-                />
-              </v-avatar>
-              <strong class="ml-3">{{ course.admin.name }}</strong>
-            </div>
-          </div>
-        </v-col>
-        <v-col id="course-buy-content">
-          <CourseBuyCard :course="course" />
-        </v-col>
-        <FAQ />
-        <br />
-      </v-row>
-    </v-container>
-    <ftr />
-  </div>
+						<div class="avtar-with-name">
+							<v-avatar size="36px">
+								<img
+									alt="Avatar"
+									src="https://avatars.githubusercontent.com/u/52647252?v=4&s=460"
+								/>
+							</v-avatar>
+							<strong class="ml-3">{{
+								course.admin.name
+							}}</strong>
+						</div>
+					</div>
+				</v-col>
+				<v-col id="course-buy-content">
+					<CourseBuyCard :course="course" />
+				</v-col>
+				<FAQ />
+				<br />
+			</v-row>
+		</v-container>
+	</div>
 </template>
-
-
 
 <script>
 import { mapGetters, mapState } from "vuex";
 
 export default {
-  async fetch({ store, params }) {
-    await store.dispatch("videos/loadAllVideoOfCourse", params.id);
-  },
-  methods: {
-    getCourseId() {
-      console.log(this.params);
-    },
+	async fetch({ store, params }) {
+		await store.dispatch("videos/loadAllVideoOfCourse", params.id);
+	},
+	methods: {
+		getCourseId() {
+			console.log(this.params);
+		},
 
-    getVideos() {
-      console.log(this.chapters);
-    },
-  },
-  computed: {
-    ...mapState({
-      chapters: (state) => state.videos.videos,
-    }),
+		getVideos() {
+			console.log(this.chapters);
+		},
+	},
+	computed: {
+		...mapState({
+			chapters: (state) => state.videos.videos,
+		}),
 
-    ...mapGetters({
-      getCourse: "courses/get",
-    }),
+		...mapGetters({
+			getCourse: "courses/get",
+		}),
 
-    course() {
-      return this.getCourse(this.$route.params.id);
-    },
-  },
+		course() {
+			return this.getCourse(this.$route.params.id);
+		},
+	},
 };
 </script>
