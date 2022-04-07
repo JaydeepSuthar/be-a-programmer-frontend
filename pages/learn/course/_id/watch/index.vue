66f915c2f30db2a066b59120d7b8b325aad1eae7
Jaydeep Suthar <technicaljade@gmail.com>
2022-04-07 15:26:31 +0530

    Learning Page Created with Server Data

--- pages/learn/course/_id/watch/index.vue
+++ pages/learn/course/_id/watch/index.vue
@@ -1,67 +1,74 @@
 <template>
-  <v-main>
-    <v-row>
-      <v-col md="9" cols="12">
-        <div
-          class="video-player-box"
-          ref="videoPlayer"
-          v-video-player:myVideoPlayer="playerOptions"
-        ></div>
-      </v-col>
-      <v-col md="3" cols="12">
-        <div class="syllabus">
-          <v-list>
-            <v-list-group v-for="chapter in chapters" :key="chapter.id">
-              <template v-slot:activator>
-                <v-list-item-title>{{
-                  chapter.chapter_name
-                }}</v-list-item-title>
-              </template>
+	<v-main>
+		<v-row>
+			<v-col md="9" cols="12">
+				<div
+					class="video-player-box"
+					ref="videoPlayer"
+					v-video-player:myVideoPlayer="playerOptions"
+				></div>
+			</v-col>
+			<v-col md="3" cols="12">
+				<div class="syllabus">
+					<v-list>
+						<v-list-group
+							v-for="chapter in chapters"
+							:key="chapter.id"
+						>
+							<template v-slot:activator>
+								<v-list-item-title>{{
+									chapter.chapter_name
+								}}</v-list-item-title>
+							</template>
 
-              <v-list-item
-                v-for="video in chapter.videos"
-                :key="video.id"
-                @click="setVideoSource(video)"
-              >
-                {{ video.title }}
-              </v-list-item>
-            </v-list-group>
-          </v-list>
-          {{ this.videoSource }}
-        </div>
-      </v-col>
-    </v-row>
-    <v-row align-content="center">
-      <v-tabs grow center-active>
-        <v-tab>Description</v-tab>
-        <v-tab>Assignment</v-tab>
-        <v-tab>Summary</v-tab>
+							<v-list-item
+								v-for="video in chapter.videos"
+								:key="video.id"
+								@click="setVideoSource(video)"
+							>
+								{{ video.title }}
+							</v-list-item>
+						</v-list-group>
+					</v-list>
+					<!-- {{ this.videoSource }} -->
+				</div>
+			</v-col>
+		</v-row>
+		<v-row align-content="center">
+			<v-col cols="9">
+				<v-tabs grow center-active>
+					<v-tab>Description</v-tab>
+					<v-tab>Assignment</v-tab>
+					<!-- <v-tab>Summary</v-tab> -->
 
-        <v-tab-item>
-          <v-crad>
-            <v-container
-              ><h1>Description</h1>
-              <p>
-                this course is about to learn programing, this course is about
-                to learn programing,this course is about to learn programing,
-                this course is about to learn programing,this course is about to
-                learn programing,this course is about to learn programing,
-              </p></v-container
-            >
-          </v-crad></v-tab-item
-        >
-        <v-tab-item>
-          <v-container
-            ><h1>Assignments</h1>
-            <p>All link will be here</p></v-container
-          >
-        </v-tab-item>
-        <v-tab-item>
-          <v-container><h1>Summary</h1></v-container>
-        </v-tab-item>
-      </v-tabs>
-    </v-row>
-  </v-main>
+					<v-tab-item>
+						<v-card>
+							<v-container
+								><h3>Description</h3>
+								<p>
+									this course is about to learn programing,
+									this course is about to learn
+									programing,this course is about to learn
+									programing, this course is about to learn
+									programing,this course is about to learn
+									programing,this course is about to learn
+									programing,
+								</p></v-container
+							>
+						</v-card></v-tab-item
+					>
+					<v-tab-item>
+						<v-container>
+							<h3>Assignments</h3>
+						</v-container>
+					</v-tab-item>
+					<!-- <v-tab-item>
+					<v-container><h3>Summary</h3></v-container>
+				</v-tab-item> -->
+				</v-tabs>
+			</v-col>
+		</v-row>
+	</v-main>
 </template>
 
 <script>
@@ -72,74 +79,77 @@ import { mapGetters, mapState } from "vuex";
 
 // If used in nuxt.js/ssr, you should keep it only in browser build environment
 if (process.browser) {
-  const VueVideoPlayer = require("vue-video-player/dist/ssr");
-  Vue.use(VueVideoPlayer);
+	const VueVideoPlayer = require("vue-video-player/dist/ssr");
+	Vue.use(VueVideoPlayer);
 
-  window.addEventListener(
-    "contextmenu",
-    function (e) {
-      // do something here...
-      e.preventDefault();
-    },
-    false
-  );
+	window.addEventListener(
+		"contextmenu",
+		function (e) {
+			// do something here...
+			e.preventDefault();
+		},
+		false
+	);
 }
 
 export default {
-  async fetch({ store, params }) {
-    await store.dispatch("videos/loadAllVideoOfCourse", params.id);
-  },
-  data() {
-    return {
-      videoSource: "",
-    };
-  },
-  methods: {
-    getCourseId() {
-      console.log(this.params);
-    },
+	async fetch({ store, params }) {
+		await store.dispatch("videos/loadAllVideoOfCourse", params.id);
+	},
+	data() {
+		return {
+			videoSource: "",
+		};
+	},
+	mounted() {
+		this.videoSource = this.chapters[0].videos[0].src;
+	},
+	methods: {
+		getCourseId() {
+			console.log(this.params);
+		},
 
-    getVideos() {
-      console.log(this.chapters);
-    },
+		getVideos() {
+			console.log(this.chapters);
+		},
 
-    async setVideoSource(video) {
-      let videoSrc = video.src;
+		async setVideoSource(video) {
+			let videoSrc = video.src;
 
-      this.videoSource = videoSrc;
+			this.videoSource = videoSrc;
 
-      // await this.$axios.post("/watch/set", { src: videoSrc });
-      // location.reload();
-    },
-  },
-  computed: {
-    ...mapState({
-      chapters: (state) => state.videos.videos,
-    }),
+			// await this.$axios.post("/watch/set", { src: videoSrc });
+			// location.reload();
+		},
+	},
+	computed: {
+		...mapState({
+			chapters: (state) => state.videos.videos,
+		}),
 
-    ...mapGetters({
-      getCourse: "courses/get",
-    }),
+		...mapGetters({
+			getCourse: "courses/get",
+		}),
 
-    course() {
-      return this.getCourse(this.$route.params.id);
-    },
+		course() {
+			return this.getCourse(this.$route.params.id);
+		},
 
-    playerOptions() {
-      return {
-        // videojs options
-        language: "en",
-        playbackRates: [0.25, 0.5, 1.0, 1.25, 1.5],
-        sources: [
-          {
-            type: "video/mp4",
-            src: `http://localhost:8000/video/${this.videoSource}`,
-          },
-        ],
-        poster: `http://localhost:8000/static/img/nodejs.png`,
-        fluid: true,
-      };
-    },
-  },
+		playerOptions() {
+			return {
+				// videojs options
+				language: "en",
+				playbackRates: [0.25, 0.5, 1.0, 1.25, 1.5],
+				sources: [
+					{
+						type: "video/mp4",
+						src: `http://localhost:8000/video/${this.videoSource}`,
+					},
+				],
+				poster: `http://localhost:8000/static/img/nodejs.png`,
+				fluid: true,
+			};
+		},
+	},
 };
 </script>
--- pages/me/learning.vue
+++ pages/me/learning.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-container>
-    <NavigationBar />
-
-    <h2 id="page__title">My Learning</h2>
-
-    <LearningList />
-  </v-container>
+	<section id="my-learning">
+		<NavigationBar />
+		<v-container>
+			<v-list>
+				<v-list-items
+					v-for="item in this.courseList"
+					:key="item.id"
+				>
+					<v-card class="mb-5" hover>
+						<v-row>
+							<v-col cols="2">
+								<v-img
+									width="200"
+									:src="`http://localhost:8000/static/img/${item.thumbnail}`"
+								></v-img>
+							</v-col>
+							<v-col cols="10">
+								<v-card-title>{{ item.title }}</v-card-title>
+								<v-card-actions>
+									<v-btn  :to="`/learn/course/${item.id}/watch`" class="primary">Continue Learning</v-btn>
+								</v-card-actions>
+							</v-col>
+						</v-row>
+					</v-card>
+				</v-list-items>
+			</v-list>
+		</v-container>
+	</section>
 </template>
-
 
 <script>
 export default {
-  name: "Learning",
-  middleware: "auth-user",
+	data: () => ({
+		courseList: [],
+	}),
+	async fetch() {
+		const response = await this.$axios(`/misc/learning`);
+		const data = await response.data.data;
+		data.map((item) => this.courseList.push(item));
+		console.log(this.courseList);
+	},
 };
 </script>
